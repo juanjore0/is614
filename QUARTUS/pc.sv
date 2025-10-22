44032e86c653b104de4160d15f1d739bbaa15325
juanjore0 <juan.arango7@utp.edu.co>
2025-10-22 16:52:39 -0500

    cambio pc, reset funcionando

--- QUARTUS/pc.sv
+++ QUARTUS/pc.sv
@@ -1,17 +1,16 @@
 module pc (
-  input[31:0] next_address,
-  input clk,
-  input reset,
-  input[31:0] initial_address,
-
-  output reg[31:0] address
+  input  logic [31:0] next_address,
+  input  logic clk,
+  input  logic reset,             // Reset activo en alto
+  input  logic [31:0] initial_address, // Primer valor de memoria (por ej. 0)
+  output logic [31:0] address
 );
 
-  always @(posedge clk) begin
+  always_ff @(posedge clk or posedge reset) begin
     if (reset)
-      address <= initial_address;
-    else 
-      address <= next_address;
+      address <= initial_address;   // Vuelve a 0 o dirección inicial
+    else
+      address <= next_address;      // Avanza normalmente
   end
-  
+
 endmodule
